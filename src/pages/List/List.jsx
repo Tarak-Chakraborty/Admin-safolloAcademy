147382afcf95c5993affe59ef245e34ce8297e20
Tarak chakraborty <144036469+Tarak-Chakraborty@users.noreply.github.com>
2024-05-08 16:10:05 +0530

    Update List.jsx

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -43,7 +43,7 @@ const List = ({url}) => {
         {list.map((item,index) => {
           return (
             <div key={index} className="list-table-format">
-              <img src={`₹{url}/images/`+item.image} alt="" />
+              <img src={`${url}/images/`+item.image} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>₹{item.price}</p>
