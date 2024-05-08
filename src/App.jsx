73c84dfbd28e0947c370de8a41cc85c74eaca86e
Tarak chakraborty <144036469+Tarak-Chakraborty@users.noreply.github.com>
2024-05-08 12:00:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://backend-safolloacademy.onrender.com";
   return (
     <div>
       <ToastContainer/>
